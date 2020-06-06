9a5df0703ec595ba23345a61fcf16d32bd6faeaa
Alexandre <debris.alex@gmail.com>
2020-06-07 01:03:28 +0200

    (test) : Add CommandHelperTest

---
+++ src/test/java/life/eclosia/commandapi/CommandHelperTest.java
@@ -0,0 +1,80 @@
+package life.eclosia.commandapi;
+
+import life.eclosia.commandapi.commands.CommandManager;
+import life.eclosia.commandapi.commands.SubCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+
+public class CommandHelperTest {
+
+    JavaPlugin javaPlugin;
+    SubCommand subCommand;
+    CommandManager commandManager;
+
+    @Before
+    public void setUp() {
+        javaPlugin = Mockito.mock(JavaPlugin.class);
+        subCommand = Mockito.mock(SubCommand.class);
+
+        CommandManager commandManager = Mockito.mock(CommandManager.class);
+        CommandHelper.setCommandManager(commandManager);
+    }
+
+    @Test
+    public void addCommandIsAllNull() {
+        CommandHelper.AddCommand(null, null, null);
+        commandManager = CommandHelper.getCommandManager();
+
+        Mockito.verify(commandManager, Mockito.times(0)).addSubCommand(subCommand);
+    }
+
+    @Test
+    public void addCommandPluginIsNull() {
+        CommandHelper.AddCommand(null, "Test", subCommand);
+        commandManager = CommandHelper.getCommandManager();
+
+        Mockito.verify(commandManager, Mockito.times(0)).addSubCommand(subCommand);
+    }
+
+    @Test
+    public void addCommandNameIsNull() {
+        CommandHelper.AddCommand(javaPlugin, null, subCommand);
+        commandManager = CommandHelper.getCommandManager();
+
+        Mockito.verify(commandManager, Mockito.times(0)).addSubCommand(subCommand);
+    }
+
+    @Test
+    public void addCommandCommandIsNull() {
+        CommandHelper.AddCommand(javaPlugin, "TEST", null);
+        commandManager = CommandHelper.getCommandManager();
+
+        Mockito.verify(commandManager, Mockito.times(0)).addSubCommand(subCommand);
+    }
+
+    @Test
+    public void addCommand() {
+        CommandHelper.AddCommand(javaPlugin, "TEST", subCommand);
+        commandManager = CommandHelper.getCommandManager();
+
+        Mockito.verify(commandManager, Mockito.times(1)).addSubCommand(subCommand);
+    }
+
+    @Test
+    public void getAllCommand() {
+        commandManager = new CommandManager();
+        CommandHelper.setCommandManager(commandManager);
+
+        int size = CommandHelper.GetAllCommand().size();
+        assertEquals(0, size);
+
+        commandManager.addSubCommand(subCommand);
+
+        size = CommandHelper.GetAllCommand().size();
+        assertEquals(1, size);
+    }
+}
